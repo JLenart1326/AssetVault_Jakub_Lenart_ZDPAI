bf2b74aba683a8e07b852aedb4a56e67943f2166
JLenart1326 <75327126+JLenart1326@users.noreply.github.com>
2025-12-23 16:52:21 +0100

    Updated logout.php with code comments

--- public/views/logout.php
+++ public/views/logout.php
@@ -1,12 +1,17 @@
 <?php
+// Dołączamy klasę User, aby skorzystać z jej metody wylogowywania
 require_once __DIR__ . '/../classes/User.php';
+
+// Upewniamy się, że sesja jest aktywna (bo musimy mieć do niej dostęp, żeby ją zniszczyć)
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+// Tworzymy obiekt użytkownika i wywołujemy metodę czyszczącą sesję i ciasteczka
 $user = new User();
 $user->logout();
 
+// Po wylogowaniu wyrzucamy użytkownika na stronę logowania
 header('Location: /login');
 exit();
 ?>
