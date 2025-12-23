66a2b4124708b39ba98f20b51b1b05e8db276a0e
JLenart1326 <75327126+JLenart1326@users.noreply.github.com>
2025-12-23 16:29:55 +0100

    Updated auth.php to support PHP Slim

--- public/auth.php
+++ public/auth.php
@@ -4,6 +4,6 @@ if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 // Sprawdzenie, czy użytkownik jest zalogowany
 if (!isset($_SESSION['user_id'])) {
     // Jeśli nie – przekierowanie do ekranu logowania
-    header('Location: views/login.php');
+    header('Location: /login');
     exit();
 }
