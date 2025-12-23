177de8b0dd264bf793a94148b1bed01e4bef0673
JLenart1326 <75327126+JLenart1326@users.noreply.github.com>
2025-12-23 16:31:40 +0100

    Updated asset_list.php to support PHP Slim

--- public/views/partials/asset_list.php
+++ public/views/partials/asset_list.php
@@ -1,14 +1,14 @@
 <?php
 if (!isset($asset)) return;
 
-$thumb = '../images/default-thumb.png';
+$thumb = '/images/default-thumb.png';
 if (!empty($asset['images'])) {
-    $thumb = '../' . htmlspecialchars($asset['images'][0]['image_path']);
+    $thumb = '/' . htmlspecialchars($asset['images'][0]['image_path']);
 }
 
 $extension = pathinfo($asset['file_path'], PATHINFO_EXTENSION);
 ?>
-<div class="asset-card-link-js" data-href="asset.php?id=<?= $asset['id'] ?>&from=<?= $source ?>" style="cursor:pointer;">
+<div class="asset-card-link-js" data-href="/asset?id=<?= $asset['id'] ?>&from=<?= $source ?>" style="cursor:pointer;">
   <div class="asset-card">
     <div class="asset-preview">
         <img src="<?= $thumb ?>" alt="Preview" />
@@ -18,7 +18,7 @@ $extension = pathinfo($asset['file_path'], PATHINFO_EXTENSION);
       <div class="asset-description"><?= htmlspecialchars($asset['description']) ?></div>
       <div class="asset-footer">
         <span class="asset-type">.<?= strtolower($extension) ?></span>
-        <a href="../<?= htmlspecialchars($asset['file_path']) ?>" download class="download-btn" onclick="event.stopPropagation();">Download</a>
+        <a href="/<?= htmlspecialchars($asset['file_path']) ?>" download class="download-btn" onclick="event.stopPropagation();">Download</a>
       </div>
     </div>
   </div>
@@ -26,11 +26,11 @@ $extension = pathinfo($asset['file_path'], PATHINFO_EXTENSION);
 
 <script>
 document.querySelectorAll('.asset-card-link-js').forEach(card => {
-  card.addEventListener('click', function(event) {
+  card.onclick = function(event) {
     if (event.target.closest('.download-btn')) {
       return;
     }
     window.location.href = this.dataset.href;
-  });
+  };
 });
 </script>
